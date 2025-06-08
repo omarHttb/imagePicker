a6eb134483b128a06ad2d80e87629605705468e8
omarhttb <omar.hattab37@Gmail.com>
2025-06-08 10:44:26 +0300

    image picking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'imagePicker';
+  selectedFileName: string = '';
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFileName = input.files[0].name;
+    }
+  }
 }
